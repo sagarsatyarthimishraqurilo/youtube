c9ceddaf1b77a44dbf8c7581187188c365a28698
sagarsatyarthimishraqurilo <sagarsatyarthimishraqurilo@gmail.com>
2025-11-21 10:37:33 +0530

    Adding Utils features

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -2,7 +2,7 @@ class ApiError extends Error {
     constructor(
         statusCode,
         message= "something went wrong",
-        error = [],
+        errors = [],
         statck = ""
     ){
         super(message)
@@ -10,7 +10,7 @@ class ApiError extends Error {
         this.data = null
         this.message = message
         this.success = false
-        this.errors = this.errors
+        this.errors = errors
 
 
         if(statck){
